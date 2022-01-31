aa023530665ed389d7756d37231e4be6c0564a91
CruellaDeVil <dev.timetravel@gmail.com>
2022-01-31 18:18:34 +0900

    Solve: LV1_가운데 글자 가져오기

---
+++ level1/가운데 글자 가져오기/code.js
@@ -0,0 +1,6 @@
+function solution(s) {
+  const length = s.length;
+  const oddCase = s.slice((length - 1) / 2, (length + 1) / 2);
+  const evenCase = s.slice((length - 2) / 2, (length + 2) / 2);
+  return (length % 2 === 1) ? oddCase : evenCase;
+}
