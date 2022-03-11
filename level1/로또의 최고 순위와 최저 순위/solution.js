f987efb09bcd4fbc4e0c939bc32fd6c55eb8b029
cruelladevil <dev.timetravel@gmail.com>
2022-03-11 19:17:26 +0900

    Solve: LV1_로또의 최고 순위와 최저 순위

---
+++ level1/로또의 최고 순위와 최저 순위/solution.js
@@ -0,0 +1,11 @@
+function solution(lottos, win_nums) {
+  const minCount = lottos.filter(number => number !== 0 && win_nums.includes(number)).length;
+  const maxCount = minCount + lottos.filter(number => number === 0).length;
+
+  function getGrade(count) {
+    const grade = 7 - count;
+    return count > 1 ? grade : 6;
+  }
+
+  return [getGrade(maxCount), getGrade(minCount)];
+}
