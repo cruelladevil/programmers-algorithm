246a633500ddd7b9acb7ec319c1879e90e578074
cruelladevil <dev.timetravel@gmail.com>
2022-03-22 21:03:20 +0900

    Refactor: early retrun 패턴을 직관적으로 배열구조로 변경

--- level2/피보나치 수/solution.js
+++ level2/피보나치 수/solution.js
@@ -1,16 +1,8 @@
 function solution(n) {
-  if (n === 0) return 0;
-  else if (n === 1) return 1;
-  else {
-    let result;     // A(n)
-    let iterA = 0;  // A(n-2)
-    let iterB = 1;  // A(n-1)
+  const fibo = [0, 1];
 
-    for (let i = 2; i <= n; i++) {
-      result = (iterA % 1234567) + (iterB % 1234567);
-      iterA = iterB;
-      iterB = result;
-    }
-    return result % 1234567;
+  for (let i = 2; i <= n; i++) {
+    fibo[i] = (fibo[i - 1] + fibo[i - 2]) % 1234567;
   }
+  return fibo[n];
 }
