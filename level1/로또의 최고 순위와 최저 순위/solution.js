fc02655574287d77c4812273bcc6d978070dd205
cruelladevil <dev.timetravel@gmail.com>
2022-04-02 23:37:49 +0900

    Refactor: 함수 분리

--- level1/로또의 최고 순위와 최저 순위/solution.js
+++ level1/로또의 최고 순위와 최저 순위/solution.js
@@ -1,11 +1,10 @@
 function solution(lottos, win_nums) {
   const minCount = lottos.filter(number => number !== 0 && win_nums.includes(number)).length;
   const maxCount = minCount + lottos.filter(number => number === 0).length;
-
-  function getGrade(count) {
-    const grade = 7 - count;
-    return count > 1 ? grade : 6;
-  }
-
   return [getGrade(maxCount), getGrade(minCount)];
+}
+
+function getGrade(count) {
+  const grade = 7 - count;
+  return count > 1 ? grade : 6;
 }
