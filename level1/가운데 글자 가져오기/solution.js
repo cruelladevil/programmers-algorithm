52b7990c129a5c183c6c29b0079474ff770b2b19
cruelladevil <dev.timetravel@gmail.com>
2022-04-13 17:49:40 +0900

    Refactor: 코드스타일 적용

--- level1/가운데 글자 가져오기/solution.js
+++ level1/가운데 글자 가져오기/solution.js
@@ -2,5 +2,5 @@ function solution(s) {
   const length = s.length;
   const oddCase = s.slice((length - 1) / 2, (length + 1) / 2);
   const evenCase = s.slice((length - 2) / 2, (length + 2) / 2);
-  return (length % 2 === 1) ? oddCase : evenCase;
+  return length % 2 === 1 ? oddCase : evenCase;
 }
