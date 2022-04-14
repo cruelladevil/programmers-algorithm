a81e7dbd0af89cfc542d95efd64794b5af42a13c
cruelladevil <dev.timetravel@gmail.com>
2022-04-14 19:11:04 +0900

    Refactor: 코드스타일 적용 및 리턴값을 변수에 할당하여 읽기 쉽게 변경

--- level1/하샤드 수/solution.js
+++ level1/하샤드 수/solution.js
@@ -1,6 +1,8 @@
 function solution(x) {
-  const digitSum = x.toString()
+  const digitSum = x
+    .toString()
     .split('')
     .reduce((acc, cur) => acc + parseInt(cur), 0);
-  return x % digitSum === 0;
+  const isHarshadNumber = x % digitSum === 0;
+  return isHarshadNumber;
 }
