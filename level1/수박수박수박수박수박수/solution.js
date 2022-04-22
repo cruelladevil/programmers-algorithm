6f45c27f3241a74617c9d56259f818bb41001e9e
cruelladevil <dev.timetravel@gmail.com>
2022-04-22 23:59:34 +0900

    Refactor: 코드스타일 적용

--- level1/수박수박수박수박수박수/solution.js
+++ level1/수박수박수박수박수박수/solution.js
@@ -1,4 +1,4 @@
 function solution(n) {
-  const isOdd = (n % 2 === 1);
-  return "수박".repeat(n / 2) + (isOdd ? "수" : "");
+  const isOdd = n % 2 === 1;
+  return '수박'.repeat(n / 2) + (isOdd ? '수' : '');
 }
--- level1/짝수와 홀수/solution.js
+++ level1/짝수와 홀수/solution.js
@@ -1,3 +1,3 @@
 function solution(num) {
-  return (num % 2 === 0) ? "Even" : "Odd";
+  return num % 2 === 0 ? 'Even' : 'Odd';
 }
