54fd460c41fc24e94e059d7b5b1dcafc3e8cb950
cruelladevil <dev.timetravel@gmail.com>
2022-04-23 23:41:42 +0900

    Refactor: 나머지 연산을 1에서 0으로, 반환하는 내용이 무엇인지 리팩토링

--- level1/가운데 글자 가져오기/solution.js
+++ level1/가운데 글자 가져오기/solution.js
@@ -2,5 +2,6 @@ function solution(s) {
   const length = s.length;
   const oddCase = s.slice((length - 1) / 2, (length + 1) / 2);
   const evenCase = s.slice((length - 2) / 2, (length + 2) / 2);
-  return length % 2 === 1 ? oddCase : evenCase;
+  const middleLetter = length % 2 === 0 ? evenCase : oddCase;
+  return middleLetter;
 }
