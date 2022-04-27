4c68ff1fd27030126ef65d8b8cf309a670ac7c31
cruelladevil <dev.timetravel@gmail.com>
2022-04-27 23:59:25 +0900

    Refactor: mapping, 구조분해, 문제 내용과 일치화(i, j, k)

--- level1/K번째수/solution.js
+++ level1/K번째수/solution.js
@@ -1,10 +1,10 @@
 function solution(array, commands) {
-  const answer = [];
-  commands.forEach((element) => {
-    const [start, end, index] = element;
-    const slicedArray = array.slice(start - 1, end);
-    const sortedArray = slicedArray.sort((a, b) => a - b);
-    answer.push(sortedArray[index - 1]);
+  const result = commands.map(([i, j, k]) => {
+    const sliced = array
+      .slice(i - 1, j)
+      .sort((a, b) => a - b);
+    return sliced[k - 1];
   });
-  return answer;
+
+  return result;
 }
