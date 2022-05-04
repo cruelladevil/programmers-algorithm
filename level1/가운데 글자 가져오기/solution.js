24433d20858036b943e3fc7171db62f144913c08
cruelladevil <dev.timetravel@gmail.com>
2022-05-04 21:36:59 +0900

    Refactor: 구조분해

--- level1/가운데 글자 가져오기/solution.js
+++ level1/가운데 글자 가져오기/solution.js
@@ -1,5 +1,5 @@
 function solution(s) {
-  const length = s.length;
+  const { length } = s;
   const oddCase = s.slice((length - 1) / 2, (length + 1) / 2);
   const evenCase = s.slice((length - 2) / 2, (length + 2) / 2);
   const middleLetter = length % 2 === 0 ? evenCase : oddCase;
