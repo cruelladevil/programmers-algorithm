20315712f9bc597e5fd27a4ee22653473f3cc579
cruelladevil <dev.timetravel@gmail.com>
2022-05-14 15:24:12 +0900

    Refactor: map -> 일반객체, for of -> forEach 변경

--- level2/오픈채팅방/solution.js
+++ level2/오픈채팅방/solution.js
@@ -1,15 +1,16 @@
 function solution(record) {
-  const users = new Map();
+  const users = {};
   const messageList = [];
   const messageType = {
     Enter: '님이 들어왔습니다.',
     Leave: '님이 나갔습니다.',
   };
 
-  for (const item of record) {
+  record.forEach((item) => {
     const [type, uid, name] = item.split(' ');
-    if (name) users.set(uid, name);
+    if (name) users[uid] = name;
     if (type !== 'Change') messageList.push([uid, messageType[type]]);
-  }
-  return messageList.map(([uid, message]) => `${users.get(uid)}${message}`);
+  });
+
+  return messageList.map(([uid, message]) => `${users[uid]}${message}`);
 }
