c78efcc2ca8046967ad88b70f37d12e77822a098
cruelladevil <dev.timetravel@gmail.com>
2022-05-15 23:33:46 +0900

    Refactor: split 과정을 생략함으로써 중간 배열 차단

--- level1/자릿수 더하기/solution.js
+++ level1/자릿수 더하기/solution.js
@@ -1,5 +1,10 @@
 function solution(n) {
-  const string = n.toString();
-  const split = string.split('');
-  return split.reduce((acc, cur) => acc + parseInt(cur), 0);
+  const numberToString = n.toString();
+  let sum = 0;
+
+  for (let i = 0; i < numberToString.length; i += 1) {
+    sum += Number(numberToString[i]);
+  }
+
+  return sum;
 }
