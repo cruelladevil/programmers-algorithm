496adf6131e6ec4a2d71121fc07ad22e8851908d
cruelladevil <dev.timetravel@gmail.com>
2022-05-21 23:58:12 +0900

    Refactor: 반복문을 고차함수로 변경, 변수명 혼용 수정, new Array -> Array.from 변경

--- level1/체육복/solution.js
+++ level1/체육복/solution.js
@@ -1,16 +1,26 @@
 function solution(n, lost, reserve) {
-  const uniformList = new Array(n).fill(1);
-  lost.forEach(number => (uniformList[number - 1] -= 1));
-  reserve.forEach(number => (uniformList[number - 1] += 1));
+  const uniformList = Array.from({ length: n }, () => 1);
 
-  for (let i = 0; i < uniformList.length; i++) {
-    if (uniformList[i] === 0 && uniformList[i - 1] === 2) {
+  lost.forEach((number) => {
+    uniformList[number - 1] -= 1;
+  });
+
+  reserve.forEach((number) => {
+    uniformList[number - 1] += 1;
+  });
+
+  uniformList.forEach((current, i) => {
+    const previous = uniformList[i - 1];
+    const next = uniformList[i + 1];
+
+    if (current === 0 && previous === 2) {
       uniformList[i - 1] -= 1;
       uniformList[i] += 1;
-    } else if (uniformList[i] === 0 && uniformList[i + 1] === 2) {
+    } else if (current === 0 && next === 2) {
       uniformList[i + 1] -= 1;
       uniformList[i] += 1;
     }
-  }
-  return uniformList.filter(number => number > 0).length;
+  });
+
+  return uniformList.filter((count) => count > 0).length;
 }
