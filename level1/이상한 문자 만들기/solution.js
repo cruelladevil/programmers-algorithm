4810d2d670971b11558564e94599e166a3047d68
cruelladevil <dev.timetravel@gmail.com>
2022-05-23 23:55:57 +0900

    Refactor: 코드 스타일 적용, 리턴값 명시

--- level1/이상한 문자 만들기/solution.js
+++ level1/이상한 문자 만들기/solution.js
@@ -1,12 +1,20 @@
 function solution(s) {
   const wordList = s.split(' ');
-  const weirdWordList = wordList.map(word => {
+  const weirdWordList = wordList.map((word) => {
     let weirdWord = '';
-    for (let i = 0; i < word.length; i++) {
-      if (i % 2 === 0) weirdWord += word[i].toUpperCase();
-      else weirdWord += word[i].toLowerCase();
+
+    for (let i = 0; i < word.length; i += 1) {
+      if (i % 2 === 0) {
+        weirdWord += word[i].toUpperCase();
+      } else {
+        weirdWord += word[i].toLowerCase();
+      }
     }
+
     return weirdWord;
   });
-  return weirdWordList.join(' ');
+
+  const weirdSentance = weirdWordList.join(' ');
+
+  return weirdSentance;
 }
