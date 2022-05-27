e72e1613de62fd0bfc169e6388534e9c779b1330
cruelladevil <dev.timetravel@gmail.com>
2022-05-27 23:09:39 +0900

    Refactor:  코드스타일 적용, getGrade 함수를 내부함수로 변경

--- level1/로또의 최고 순위와 최저 순위/solution.js
+++ level1/로또의 최고 순위와 최저 순위/solution.js
@@ -1,10 +1,12 @@
 function solution(lottos, win_nums) {
-  const minCount = lottos.filter(number => number !== 0 && win_nums.includes(number)).length;
-  const maxCount = minCount + lottos.filter(number => number === 0).length;
-  return [getGrade(maxCount), getGrade(minCount)];
-}
+  const minCount = lottos.filter((number) => number !== 0 && win_nums.includes(number)).length;
+  const maxCount = minCount + lottos.filter((number) => number === 0).length;
 
-function getGrade(count) {
-  const grade = 7 - count;
-  return count > 1 ? grade : 6;
+  const getGrade = (count) => {
+    const grade = 7 - count;
+
+    return count > 1 ? grade : 6;
+  };
+
+  return [getGrade(maxCount), getGrade(minCount)];
 }
